36e773f6d2f157b373cccaa72c984d2a6e012aa1
Stanislav <avgustwot@mail.ru>
2023-03-11 21:06:34 +0300

    Задача 6

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 6
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 != 0)
+        Console.WriteLine("неЧетное");
+else 
+        Console.WriteLine("Четное");
